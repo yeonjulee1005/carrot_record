735fc341498a350ae332eb3d2c70f67846f4d591
Yeonju Lee <yeonju.lee1005@gmail.com>
2022-02-20 20:46:28 +0900

    intro 작업

--- lib/screens/start/intro.dart
+++ lib/screens/start/intro.dart
@@ -1,5 +1,6 @@
 import 'package:carrot_record/utils/logger.dart';
 import 'package:extended_image/extended_image.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class IntroPage extends StatelessWidget {
@@ -11,22 +12,63 @@ class IntroPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Padding(padding: EdgeInsets.all(30)),
-        Text('당근 마켓'),
-        ExtendedImage.asset('assets/images/carrot_intro.png'),
-        Text('우리 동네 중고 직거래 당근마켓'),
-        Text('''당근마켓은 동네 직거래 마켓이에요.
-내 동네를 설정하고 시작해보세요.'''),
-        TextButton(
-            onPressed: onButtonClick,
-            child: Text('내 동내 설정하고 시작하기',
-              style: TextStyle(color: Colors.white)
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text('당근 마켓',
+                style: TextStyle(
+                    fontSize: 36,
+                    color: Color(0xffD98442),
+                    fontWeight: FontWeight.w700),
+                textAlign: TextAlign.center,
+              ),
             ),
-        style: TextButton.styleFrom(
-            backgroundColor: Colors.teal))
-      ],
+            ExtendedImage.asset('assets/images/carrot_intro.png'),
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text('우리 동네 중고 직거래 당근마켓',
+                style: TextStyle(
+                    fontSize: 26,
+                    fontWeight: FontWeight.w600),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text('''당근마켓은 동네 직거래 마켓이에요.
+내 동네를 설정하고 시작해보세요.''',
+                style: TextStyle(
+                    fontSize: 15),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextButton(
+                      onPressed: onButtonClick,
+                      child: const Text('내 동내 설정하고 시작하기',
+                        style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.white
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                  style: TextButton.styleFrom(
+                      backgroundColor: Colors.teal)),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
